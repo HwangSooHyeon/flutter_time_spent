5e15e7151ad06bfaeacf36a2af936046e0cb282b
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2023-02-05 17:16:27 +0900

    feat(home_screen): 임시 텍스트에 텍스트 테마 적용

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -26,23 +26,30 @@ class HomeScreen extends StatelessWidget {
 class _DDay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final textTheme = Theme.of(context).textTheme;
+
     return Column(
       children: [
         const SizedBox(height: 16.0),
         Text(
           'U&I',
+          style: textTheme.headlineLarge,
         ),
         const SizedBox(height: 16.0),
         Text(
           '우리 처음 만난 날',
+          style: textTheme.bodyLarge,
         ),
         Text(
           '2021.11.13',
+          style: textTheme.bodyMedium,
         ),
         const SizedBox(height: 16.0),
         IconButton(iconSize: 60.0, onPressed: () {}, icon: Icon(Icons.favorite,),),
         Text(
           'D+365',
+          style: textTheme.headlineMedium,
         ),
       ],
     );
